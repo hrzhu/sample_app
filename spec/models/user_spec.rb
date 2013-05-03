898e6f9a23c6b63129f7bd5a8a6922212e4379a6
hrzhu <a@hrzhu.me>
2013-05-04 00:10:27 +0800

    alidate the presence of the name and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,15 @@ describe User do
   subject { @user }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
 end
