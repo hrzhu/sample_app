03185f16dd403ee018902920968524b9a5320208
hrzhu <a@hrzhu.me>
2013-05-16 15:20:32 +0800

    update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 end
