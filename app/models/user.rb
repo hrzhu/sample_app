47966f507f1536b1e19016d588f0ce284c3cfbb9
hrzhu <a@hrzhu.me>
2013-05-18 16:43:12 +0800

    a proto-feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,10 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
   def create_remember_token
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -135,6 +135,16 @@ describe User do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     end
 
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
+
     it "should have the right microposts in the right order" do
       @user.microposts.should == [newer_micropost, older_micropost]
     end
