12b5fdf2b93e56f77863cf8e84e4b745091c017f
hrzhu <a@hrzhu.me>
2013-05-18 16:03:27 +0800

    ensure only the signed in users can create or destroy microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,10 +48,6 @@ class UsersController < ApplicationController
 
   private
 
-    def signed_in_user
-      store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
 
     def correct_user
       @user = User.find(params[:id])
