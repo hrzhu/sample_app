c33cc250d0d7ed7684edfea1d73757668515c4dc
hrzhu <a@hrzhu.me>
2013-05-20 13:22:16 +0800

    stats and a follow form

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,21 +1,15 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  subject { page }
+
   describe "Home page" do
     before { visit root_path }
 
-    it "should have the h1 'Sample App'" do
-      page.should have_selector('h1', text: 'Sample App')
-    end
-
-    it "should have the base title" do
-      page.should have_selector('title',
-                                text: full_title(''))
-    end
-
-    it "should not have a custom page title" do
-      page.should_not have_selector('title', text: 'Home')
-    end
+    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
 
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
@@ -31,45 +25,53 @@ describe "Static pages" do
           page.should have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
   end
 
   describe "Help page" do
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', text: 'Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title',
-                                text: full_title('Help'))
-    end
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
-    it "should have the h1 'About'" do
-      visit about_path
-      page.should have_selector('h1', text: 'About Us')
-    end
+    before { visit about_path }
 
-    it "should have the title 'About Us'" do
-      visit about_path
-      page.should have_selector('title',
-                                text: full_title('About Us'))
-    end
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
-    end
+    before { visit contact_path }
 
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',
-                                text: full_title('Contact'))
-    end
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'h1', text: 'Sample App'
   end
 end
