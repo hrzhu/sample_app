694fcd6c93667f243f18467be560c554e2b72bb1
hrzhu <a@hrzhu.me>
2013-05-20 10:05:56 +0800

    followed users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,9 +27,28 @@ describe User do
 
   it { should respond_to(:microposts) }
   it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
 
   it { should be_valid }
   it { should_not be_admin }
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
 
   describe "with admin attribute set to 'true'" do
     before do
