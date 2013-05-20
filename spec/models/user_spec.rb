ac88ab7416cdda4426c9f913e94cd119595d5c21
hrzhu <a@hrzhu.me>
2013-05-20 10:12:24 +0800

    followers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,6 +42,11 @@ describe User do
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
 
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
     describe "and unfollowing" do
       before { @user.unfollow!(other_user) }
 
