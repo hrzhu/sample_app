415e84f5cf9c1192b61ecee1d85e31b6cc9c1c3f
hrzhu <a@hrzhu.me>
2013-05-20 13:55:25 +0800

    a first feed implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
   before_save :create_remember_token
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
